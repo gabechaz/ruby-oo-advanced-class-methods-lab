603ca4723abd0ae7cb31eff0a8345f9f97a07193
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-21 01:45:25 -0500

    commit tree 0e2b4cedc6d8569acae8b807a00611279fa1ff09

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +10,60 @@ class Song
   def save
     self.class.all << self
   end
+
+def self.create
+  song = Song.new
+  song.save
+  song
+end
+def self.new_by_name(song_name)
+  song = self.new
+    song.name = song_name
+    song
+end
+def  self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+end
+  def self.find_by_name(name)
+    self.all.select{|song| song.name == name}[0]
+  end
+def self.find_or_create_by_name(arg)
+  if self.find_by_name(arg)
+    self.find_by_name(arg)
+  else
+    self.create_by_name(arg)
+  end
+end
+def self.alphabetical
+  self.all.sort_by {|s| s.name}
+end
+
+def self.new_from_filename(filename)
+  parts = filename.split(" - ")
+  artist_name = parts[0]
+  song_name = parts[1].gsub(".mp3", "")
+
+  song = self.new
+  song.name = song_name
+  song.artist_name = artist_name
+  song
+end
+
+def self.create_from_filename(filename)
+  parts = filename.split(" - ")
+  artist_name = parts[0]
+  song_name = parts[1].gsub(".mp3", "")
+
+  song = self.create
+  song.name = song_name
+  song.artist_name = artist_name
+  song
+end
+
+def self.destroy_all
+  self.all.clear
+end
 
 end
